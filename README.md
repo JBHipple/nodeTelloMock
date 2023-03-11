1df98e1e205bab2c47bce81598f636c20b0c8592
Josh Hipple <jbhipple@gmail.com>
2023-03-11 01:11:34 -0600

    Initial server setup, contains basic command validation

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # nodeTelloMock
 Mock test server for DJI Tello interface development
+
+NodeJS based UDP server that responds to DJI Tello commands based on SDK 3.0
+The UDP server will only positively respond to commands outlined by the DJI Tello SDK v3.0
+
+Coming soon: Docker contanier support
---
+++ telloUdp.js
@@ -0,0 +1,114 @@
+var udp = require("dgram");
+
+var server = udp.createSocket("udp4");
+
+var port = 8889;
+
+var serverState = {
+    'commandOn': false,
+    'currentlyFlying': false,
+    'currentSpeed': 0,
+    'battery': 100
+};
+
+server.on("listening", function(){
+    var address = server.address();
+    console.log("Listening to ", "Address: ", address.address, "Port: ", address.port);
+});
+
+server.on("message", function(message, info){
+    var command = message.toString();
+
+    console.log("Command received: " + message.toString());
+
+    var commandState = validateCommand(command);
+
+    var response
+    if(commandState.valid){
+        response = Buffer.from("OK");
+    } else{
+        response = Buffer.from("ERROR: " + commandState.message);
+    }
+
+    server.send(response, info.port, info.address, function(err){
+        if(err){
+            console.log("Sending response failed: ", err);
+            client.close();
+        } else{
+            console.log("Response sent successfully.");
+        }
+    });
+});
+
+server.on("error",function(error){
+    console.log("Error: " + error);
+    server.close();
+});
+
+server.bind(port);
+
+function validateCommand(command){
+    // Define return object
+    var retObject = {};
+
+    // Define arrays of valid commands according to DJI Tello SDK 3.0
+    var noParamCommands = ['command', 'takeoff', 'land', 'streamon', 'streamoff', 'emergency', 'motoron', 'motoroff',
+                           'throwfly', 'stop', 'reboot', 'mon', 'moff'];
+    var paramCommands = ['up', 'down', 'left', 'right', 'forward', 'back', 'cw', 'ccw', 'flip', 'go', 'curve', 'jump',
+                         'speed', 'rc', 'wifi', 'mdirection', 'ap', 'wifisetchannel', 'port', 'setfps', 'setbitrate', 'setresolution', 'multiwifi'];
+    var readCommands = ['speed?', 'battery?', 'time?', 'wifi?', 'sdk?', 'sn?', 'hardware?', 'wifiversion?', 'ap?', 
+                        'ssid?'];
+    
+    // If the command is restartServer, reset the server params and return valid
+    if(command == 'restartServer'){
+        serverState = {
+            'commandOn': false,
+            'currentlyFlying': false,
+            'currentSpeed': 0,
+            'battery': 100
+        };
+        retObject.valid = true;
+    }
+    
+    // If the passed in command contains a space, it is a command that contains a 
+    // parameter and needs to be split into each component
+    // Else, command is no-param and validation is simply checking against the list
+    if(command.includes(' ')){
+        // Split the command into its components
+        var commandArr = command.split(' ');
+
+        // Check if the command name matches one in the list of valid parameter commands
+        // Else, check if the command is in the list of no-param commands and has a param by error
+        // Else, check if the command is in the list of read commands and has a param by error
+        if(paramCommands.includes(commandArr[0])){
+            // TODO - Add logic to check against each command's required parameters
+            //validateParameters(commandArr);
+
+            retObject.valid = true;
+        } else if(noParamCommands.includes(commandArr[0]) || readCommands.includes(commandArr[0])){
+            // The command that was passed in contained a parameter and should not have one
+            retObject.valid = false;
+            retObject.message = 'Command ' + commandArr[0] + ' should not have a parameter.';
+        }
+    } else{
+        if(noParamCommands.includes(command) || readCommands.includes(command)){
+            retObject.valid = true;
+        } else{
+            // Set validity to false
+            retObject.valid = false;
+
+            // Determine reason for invalidity.  Either a param was required, or the command doesn't exist
+            if(paramCommands.includes(command)){
+                retObject.message = 'Command ' + command + ' requires parameters';
+            } else{
+                retObject.message = 'Command ' + command + ' is not valid';
+            }
+        }
+    }
+
+    if(retObject.valid == null){
+        retObject.valid = false;
+        retObject.message = 'Command ' + command + ' did not evaluate...';
+    }
+    return retObject;
+}
